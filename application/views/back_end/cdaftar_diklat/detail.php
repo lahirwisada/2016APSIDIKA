18b979160e9d7d4b978ec054b774abc3d24037b6
hajigadung <hajigadung@gmail.com>
2016-04-08 04:37:50 +0700

    add views detail

--- application/views/back_end/cdaftar_diklat/detail.php
+++ application/views/back_end/cdaftar_diklat/detail.php
@@ -1,4 +1,6 @@
 <?php
+$header_title = isset($header_title) ? $header_title : '';
+$active_modul = isset($active_modul) ? $active_modul : 'none';
 $detail = isset($detail) ? $detail : FALSE;
 ?>
 
@@ -9,7 +11,7 @@ $detail = isset($detail) ? $detail : FALSE;
             <div class="panel panel-default">
 
                 <div class="panel-heading">
-                    <h3 class="panel-title">Formulir <strong>Jenis Diklat</strong></h3>
+                    <h3 class="panel-title">Formulir <strong><?php echo $header_title; ?></strong></h3>
                 </div>
                 <div class="panel-body">
                     <p><?php echo load_partial("back_end/shared/attention_message"); ?></p>
@@ -17,29 +19,29 @@ $detail = isset($detail) ? $detail : FALSE;
                 <div class="panel-body">
 
                     <div class="form-group">
-                        <label class="col-md-3 col-xs-12 control-label">Jenis Diklat *</label>
+                        <label class="col-md-3 col-xs-12 control-label">Nama SKPD *</label>
                         <div class="col-md-6 col-xs-12">                                            
                             <div class="input-group">
-                                <input type="text" name="jenis_diklat" class="form-control" value="<?php echo $detail ? $detail->jenis_diklat : ""; ?>">
+                                <input type="text" name="nama_skpd" class="form-control" value="<?php echo $detail ? $detail->nama_skpd : ""; ?>">
                             </div>                                            
                             <span class="help-block"></span>
                         </div>
                     </div>
-
+                    
                     <div class="form-group">
-                        <label class="col-md-3 col-xs-12 control-label">Keterangan</label>
+                        <label class="col-md-3 col-xs-12 control-label">No. urut</label>
                         <div class="col-md-6 col-xs-12">                                            
                             <div class="input-group">
-                                <textarea name="keterangan" class="form-control"><?php echo $detail ? $detail->keterangan : ""; ?></textarea>
+                                <input type="text" name="col_order" class="form-control" value="<?php echo $detail ? $detail->col_order : ""; ?>">
                             </div>                                            
-                            <span class="help-block"></span>
+                            <span class="help-block">Isikan Sesuai dengan urutan yang diinginkan.<br />Isian ini digunakan untuk memudahkan pengurutan ketika menampilkan data.</span>
                         </div>
                     </div>
 
                 </div>
                 <div class="panel-footer">
                     <button type="submit" class="btn-primary btn pull-right">Submit</button>
-                    <a href="<?php echo base_url("back_end/cjenis_diklat/index"); ?>" class="btn-default btn">Batal / Kembali</a>
+                    <a href="<?php echo base_url("back_end/".$active_modul."/index"); ?>" class="btn-default btn">Batal / Kembali</a>
                 </div>
             </div>
         </form>
