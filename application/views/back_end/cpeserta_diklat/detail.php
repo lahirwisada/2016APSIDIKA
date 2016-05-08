fabfc1efea2447167601d1be633c95576c74c31e
hajigadung <hajigadung@gmail.com>
2016-05-09 04:25:41 +0700

    add nip

--- application/views/back_end/cpeserta_diklat/detail.php
+++ application/views/back_end/cpeserta_diklat/detail.php
@@ -19,21 +19,26 @@ $detail = isset($detail) ? $detail : FALSE;
                 <div class="panel-body">
 
                     <div class="form-group">
-                        <label class="col-md-3 col-xs-12 control-label">Nama SKPD *</label>
-                        <div class="col-md-6 col-xs-12">                                            
-                            <div class="input-group">
-                                <input type="text" name="nama_skpd" class="form-control" value="<?php echo $detail ? $detail->nama_skpd : ""; ?>">
-                            </div>                                            
+                        <label class="col-md-3 col-xs-12 control-label">NIP *</label>
+                        <div class="col-md-6 col-xs-12">
+                            <input type="text" name="nip" class="form-control" value="<?php echo $detail ? $detail->nip : ""; ?>">                               
                             <span class="help-block"></span>
                         </div>
                     </div>
-                    
+
+                    <div class="form-group">
+                        <label class="col-md-3 col-xs-12 control-label">Nama Depan *</label>
+                        <div class="col-md-6 col-xs-12">
+                            <input type="text" name="nama_skpd" class="form-control" value="<?php echo $detail ? $detail->nama_skpd : ""; ?>">
+
+                            <span class="help-block"></span>
+                        </div>
+                    </div>
+
                     <div class="form-group">
                         <label class="col-md-3 col-xs-12 control-label">No. urut</label>
-                        <div class="col-md-6 col-xs-12">                                            
-                            <div class="input-group">
-                                <input type="text" name="col_order" class="form-control" value="<?php echo $detail ? $detail->col_order : ""; ?>">
-                            </div>                                            
+                        <div class="col-md-6 col-xs-12">     
+                            <input type="text" name="col_order" class="form-control" value="<?php echo $detail ? $detail->col_order : ""; ?>">                                         
                             <span class="help-block">Isikan Sesuai dengan urutan yang diinginkan.<br />Isian ini digunakan untuk memudahkan pengurutan ketika menampilkan data.</span>
                         </div>
                     </div>
@@ -41,7 +46,7 @@ $detail = isset($detail) ? $detail : FALSE;
                 </div>
                 <div class="panel-footer">
                     <button type="submit" class="btn-primary btn pull-right">Submit</button>
-                    <a href="<?php echo base_url("back_end/".$active_modul."/index"); ?>" class="btn-default btn">Batal / Kembali</a>
+                    <a href="<?php echo base_url("back_end/" . $active_modul . "/index"); ?>" class="btn-default btn">Batal / Kembali</a>
                 </div>
             </div>
         </form>
