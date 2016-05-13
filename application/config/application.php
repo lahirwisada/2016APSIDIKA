7efeb2b47873f0b2651d652eed61a6b2d4512a0b
hajigadung <hajigadung@gmail.com>
2016-05-14 01:25:59 +0700

    rearrange and add using_insert_and_update_properties configuration

--- application/config/application.php
+++ application/config/application.php
@@ -13,11 +13,6 @@ $config['hashed'] = 'VFUUl2rWS6I5EdSFU2JJyQ==';
 
 $config['appkey'] = '1029384756';
 
-$config['default_limit_row'] = 20;
-$config['limit_key_param'] = 'limit';
-$config['offset_key_param'] = 'offset';
-$config['keyword_key_param'] = 'keyword';
-
 $config['lmanuser.usingbackendfrontend'] = FALSE;
 $config['user_id_column_name'] = "id_user";
 $config['profil_id_column_name'] = "id_profil";
@@ -43,3 +38,21 @@ $config['paging_using_template_name'] = TRUE;
 
 $config["pdf_paper_size"] = 'A5';
 $config["pdf_paper_orientation"] = 'L';
+
+
+/**
+ * core/LW_Model.php
+ * 
+ */
+$config['using_insert_and_update_properties'] = TRUE;
+
+$config['created_date'] = 'created_date';
+$config['modified_date'] = 'modified_date';
+$config['created_by'] = 'created_by';
+$config['modified_by'] = 'modified_by';
+$config['record_active'] = 'record_active';
+
+$config['default_limit_row'] = 20;
+$config['limit_key_param'] = 'limit';
+$config['offset_key_param'] = 'offset';
+$config['keyword_key_param'] = 'keyword';
